15c0224093ef5a2048c633348bf1aa03dbe4ab61
Yuri Skripal <yjura@protonmail.com>
2024-04-22 14:42:27 +0300

    feat: add loader & adaptive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -17,6 +17,7 @@ import {HttpClientModule} from "@angular/common/http";
         NgOptimizedImage
     ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
